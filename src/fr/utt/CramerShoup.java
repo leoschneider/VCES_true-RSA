a74e729a2b5a97f3b1420408ec74126947eedd11
Schneider léo <leo.schneider@utt.fr>
2017-11-30 12:03:39 +0100

    key-generation and user interface

--- src/fr/utt/CramerShoup.java
+++ src/fr/utt/CramerShoup.java
@@ -1,46 +1,44 @@
 package fr.utt;
 
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Array;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Random;
+import java.util.*;
 
 public class CramerShoup {
 
     private Random random = new Random();
-    private BigInteger[] privateKey;
+    private BigInteger[] privateKey; //il s'agit bien de tableaux de valeurs et pas d'une seule
     private BigInteger[] publicKey;
 
-    private void keygen(){
+    public void keygen(int nbBits){
 
-        BigInteger p = new BigInteger(128,random);
-        //on crée p random mais sur 128 bits
-        BigInteger alfa1 = new BigInteger(128, random);
-        BigInteger alfa2 = new BigInteger(128, random);
-        while(alfa1.compareTo(p)>-1 || alfa1.compareTo(new BigInteger("2"))<1)
-            alfa1 = new BigInteger(128,random);
+        BigInteger p = new BigInteger(nbBits,random); //on crée p random mais sur nbBits bits
+        p = p.nextProbablePrime(); //p devient le prochain nombre probablement premier après p avec prb 2^-100
 
-        while(alfa2.compareTo(p)>-1 || alfa2.compareTo(new BigInteger("2"))<1)
-            alfa2 = new BigInteger(128,random);
+        BigInteger alfa1 = trouverGenerateurRandom(p,nbBits);
+        BigInteger alfa2 = trouverGenerateurRandom(p,nbBits);
 
-        //déterminer alfa 1 et 2 et si ils peuvent être des générateurs
+        BigInteger x1 = new BigInteger(nbBits,random);
+        BigInteger x2 = new BigInteger(nbBits,random);
+        BigInteger y1 = new BigInteger(nbBits,random);
+        BigInteger y2 = new BigInteger(nbBits,random);
+        BigInteger w = new BigInteger(nbBits,random);
 
-        BigInteger x1 = new BigInteger(128,random);
-        BigInteger x2 = new BigInteger(128,random);
-        BigInteger y1 = new BigInteger(128,random);
-        BigInteger y2 = new BigInteger(128,random);
-        BigInteger w = new BigInteger(128,random);
-
-        BigInteger X = (alfa1.modPow(x1,p)).multiply((alfa2.modPow(x2,p)));
-        BigInteger Y = (alfa1.modPow(y1,p)).multiply((alfa2.modPow(y2,p)));
+        BigInteger X = (alfa1.modPow(x1,p)).multiply((alfa2.modPow(x2,p))).mod(p);
+        BigInteger Y = (alfa1.modPow(y1,p)).multiply((alfa2.modPow(y2,p))).mod(p);
         BigInteger W = alfa1.modPow(w,p);
 
-        publicKey = new BigInteger[]{alfa1,alfa2,X,Y,W,p};
-        privateKey = new BigInteger[]{x1,x2,y1,y2,w,p};
+        publicKey = new BigInteger[]{p,alfa1,alfa2,X,Y,W};
+        privateKey = new BigInteger[]{x1,x2,y1,y2,w};
+
+        System.out.println("La clé privée est : \n"+x1+"\n"+x2+"\n"+y1+"\n"+y2+"\n"+w+"\n"+p+"\n\n");
+        System.out.println("La clé publique est : \n"+alfa1+"\n"+alfa2+"\n"+X+"\n"+Y+"\n"+W+"\n"+p+"\n\n");
     }
 
-    public BigInteger[] encrypt(BigInteger m,BigInteger[] myPublicKey) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+    public BigInteger[] encrypt(int nbBits,BigInteger m,BigInteger[] myPublicKey) throws NoSuchAlgorithmException, UnsupportedEncodingException {
 
         BigInteger alfa1 = myPublicKey[0];
         BigInteger alfa2 = myPublicKey[1];
@@ -49,7 +47,7 @@ public class CramerShoup {
         BigInteger W = myPublicKey[4];
         BigInteger p = myPublicKey[5];
 
-        BigInteger b = new BigInteger(128,random);
+        BigInteger b = new BigInteger(nbBits,random);
         b = b.mod(p);
 
         BigInteger B1 = alfa1.modPow(b,p);
@@ -65,11 +63,71 @@ public class CramerShoup {
 
         BigInteger v = X.modPow(b,p).multiply((Y.modPow(b.multiply(beta),p)));
 
+        System.out.println("Le chiffré est : \n" +
+                B1+"\n"+B2+"\n"+c+"\n"+v+"\n");
         return new BigInteger[]{B1,B2,c,v};
     }
 
     public String decrypt(String stringInput){
         return "";
+    }
+
+    private BigInteger trouverGenerateurRandom(BigInteger p,int nbBits){
+
+        do{
+            //on cherche un nombre random plus petit que p mais sur nbBits bits
+            BigInteger numTest;
+            do{
+                numTest = new BigInteger(nbBits,random);
+            }while(numTest.compareTo(p)>0);
+            if(numTest.modPow(p.subtract(BigInteger.ONE).divide(new BigInteger("2")), p).compareTo(BigInteger.ONE) > 0)
+                return numTest;
+        }while(true);
+
+
+    }
+
+    //fonction qui sélectionne un générateur avec la liste des générateurs
+    private BigInteger[] trouverGen(BigInteger p) {
+        ArrayList<BigInteger> listeGen = creerListeGen(p,10000);
+        Collections.shuffle(listeGen,random);
+        return new BigInteger[]{listeGen.get(0),listeGen.get(1)};
+    }
+
+    private ArrayList<BigInteger> creerListeGen(BigInteger primeP,int tailleMax) {
+        //tailleMax pour empécher le système de freeze en cherchant trop de générateurs
+        ArrayList<BigInteger> generateurs = new ArrayList<>();
+        BigInteger numTest = new BigInteger("2");
+        BigInteger exponant = primeP.subtract(BigInteger.ONE).divide(new BigInteger("2"));
+        do{
+            if(numTest.modPow(exponant, primeP).compareTo(BigInteger.ONE) > 0 &&
+                    numTest.compareTo(new BigInteger("100000000000000000000"))>0)
+                //on cherche des générateurs pas trop petits d'ou 100000000000000000000
+                generateurs.add(numTest);
+            else
+                generateurs.add(numTest.negate().mod(primeP));
+            numTest = numTest.add(BigInteger.ONE);
+        }while((--tailleMax > 0) && (numTest.compareTo(primeP.subtract(BigInteger.ONE)) < 0));
+        return generateurs;
+    }
+
+    //fonction utilitaire de sqrt pour BigInteger
+    public static BigInteger sqrt(BigInteger x) {
+        BigInteger div = BigInteger.ZERO.setBit(x.bitLength()/2);
+        BigInteger div2 = div;
+        // Loop until we hit the same value twice in a row, or wind
+        // up alternating.
+        for(;;) {
+            BigInteger y = div.add(x.divide(div)).shiftRight(1);
+            if (y.equals(div) || y.equals(div2))
+                return y;
+            div2 = div;
+            div = y;
+        }
+    }
+
+    public BigInteger[] getPublicKey() {
+        return publicKey;
     }
 
 
--- src/fr/utt/Main.java
+++ src/fr/utt/Main.java
@@ -1,8 +1,56 @@
 package fr.utt;
 
-public class Main {
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main {
+    public static void main(String[] args) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        Scanner scanner = new Scanner(System.in);
+        CramerShoup cramerShoup = new CramerShoup();
+        Hash hash = new Hash();
+        int reponse=0;
+
+        System.out.println("Bienvenue dans notre Projet GS15 Léo Schneider/Marc Lanois\n");
+
+        while(reponse!=7){
+            System.out.println("Quelle action voulez vous faire ? \n" +
+                    "1 : chiffrement symétrique\n" +
+                    "2 : déchiffrement symétrique\n" +
+                    "3 : génération de clés asymétrique\n" +
+                    "4 : chiffrement asymétrique\n" +
+                    "5 : déchiffrement asymétrique\n" +
+                    "6 : hashage\n" +
+                    "7 : quitter\n");
+
+            reponse = scanner.nextInt();
+            switch (reponse){
+                case 1:
+                    //call partie Marc
+                    break;
+                case 2:
+                    //call partie Marc
+                    break;
+                case 3:
+                    System.out.println("Sur combien de bits voulez vous générer les clés ?");
+                    int nbBits=scanner.nextInt();
+                    cramerShoup.keygen(nbBits);
+                    break;
+                case 4:
+                    System.out.println("Sur combien de bits voulez vous chiffrer ?");
+                    nbBits=scanner.nextInt();
+                    cramerShoup.keygen(nbBits);
+                    System.out.println("Entrez la valeur du nombre que vous voulez chiffrer\n");
+                    cramerShoup.encrypt(nbBits,scanner.nextBigInteger(),cramerShoup.getPublicKey());
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    //lancer la fonction de hash depuis objet hash
+                    break;
+                default:break;
+            }
+        }
+        System.out.println("Au revoir.");
     }
 }
