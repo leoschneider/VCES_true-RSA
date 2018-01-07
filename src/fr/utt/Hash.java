fa4d26fa1ee43c059647cc08bc10088145ba86dd
Léo Schneider <leo.schneider@utt.fr>
2018-01-07 12:42:45 +0100

    chunk text encryption

--- src/fr/utt/Hash.java
+++ src/fr/utt/Hash.java
@@ -4,30 +4,51 @@ import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 
 public class Hash {
 
     //hash avec c comme BigInteger
-    public static BigInteger monHash(BigInteger B1,BigInteger B2,BigInteger c) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+    public static BigInteger monHash(BigInteger B1,BigInteger B2,BigInteger c) throws NoSuchAlgorithmException {
         MessageDigest crypt = java.security.MessageDigest.getInstance("SHA-256");
         crypt.reset();
+
         //on hash la concatenation des 3 valeurs B1,B2,c
-        crypt.update((B1.toString() + B2.toString() + c.toString()).getBytes("UTF-8"));
+        crypt.update((B1.toString() + B2.toString() + c.toString()).getBytes());
+
         //on récupère le hash sous forme binaire et on le recrée en objet BigInteger pour que cela soit plus simple
         byte[] betaByte = crypt.digest();
         return new BigInteger(betaByte);
     }
 
     //hash avec c comme string
-    public static BigInteger monHash(BigInteger B1,BigInteger B2,String c) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+    public static BigInteger monHash(BigInteger B1,BigInteger B2,String c) throws NoSuchAlgorithmException{
         MessageDigest crypt = java.security.MessageDigest.getInstance("SHA-256");
         crypt.reset();
         //on hash la concatenation des 3 valeurs B1,B2,c
-        crypt.update((B1.toString() + B2.toString() + c).getBytes("UTF-8"));
+        crypt.update((B1.toString() + B2.toString() + c).getBytes());
         //on récupère le hash sous forme binaire et on le recrée en objet BigInteger pour que cela soit plus simple
         byte[] betaByte = crypt.digest();
         return new BigInteger(betaByte);
     }
+
+    //hash avec c comme ArrayList de BigInteger (pour le hash de chiffrement)
+    public static BigInteger monHash(BigInteger B1,BigInteger B2,ArrayList<BigInteger> arrayListC) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        MessageDigest crypt = java.security.MessageDigest.getInstance("SHA-256");
+        crypt.reset();
+        //on hash la concatenation des 3 valeurs B1,B2,c
+
+        //on assemble les nombres entre eux
+        crypt.update(B1.toString().getBytes());
+        crypt.update(B2.toString().getBytes());
+        for(BigInteger bg: arrayListC)
+            crypt.update(bg.toString().getBytes());
+
+        //on récupère le hash sous forme binaire et on le recrée en objet BigInteger pour que cela soit plus simple
+        byte[] betaByte = crypt.digest();
+        return new BigInteger(betaByte);
+    }
+
 
     //hash juste une string
     public static BigInteger monHash(String c) throws NoSuchAlgorithmException, UnsupportedEncodingException {
